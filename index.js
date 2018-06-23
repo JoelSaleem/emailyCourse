40df0413f81f49983c88a5956fefba0a12b7bf07
Joel Saleem <joelsaleem@Joels-MacBook-Pro.local>
2018-06-23 10:46:48 +0100

    changed greeting

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 
 // Route Handler
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ bye: 'bye' });
 });
 
 // use the port that Heroku assigns, if none assigned, use 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// https://fast-ocean-54045.herokuapp.com/
